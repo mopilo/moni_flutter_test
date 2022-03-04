1d1c9432a70457186d165f8bb0dd445b9d23bff9
mopah emma <mopahemma@gmail.com>
2022-03-04 14:03:16 +0100

    add plugins - screen util, flutter bloc, equatable make my_cluster_screen default screen on launch implement plugins added

---
+++ lib/feature/my_cluster/presentation/pages/my_cluster_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyCluster extends StatefulWidget {
+  const MyCluster({Key? key}) : super(key: key);
+
+  @override
+  State<MyCluster> createState() => _MyClusterState();
+}
+
+class _MyClusterState extends State<MyCluster> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
